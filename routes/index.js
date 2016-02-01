98bfd0a301f88e0beabff74c1ff3f2c2d8524a89
JJXN <jjxn@hotmail.ca>
2016-02-01 15:12:54 -0800

    added html register page: proof of concept

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var path    = require("path");
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('login', { title: 'Express' });
+  res.render('index', { title: 'Express' });
+});
+
+/* GET login page. */
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'Express-login' });
+});
+
+/* GET register page. */
+router.get('/register', function(req, res, next) {
+  //res.render('register.html', { title: 'Express-register' });
+  res.sendFile(path.join(__dirname+'/register.html'));
 });
 
 module.exports = router;
