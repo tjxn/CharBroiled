61263c5d39b78cb5cf69aa4205bb7ad5f81affca
Trevor <trevor@trevorjackson.ca>
2016-02-02 15:26:56 -0800

    Working to implement Stormpath user management

---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var stormpath = require('express-stormpath');
+
+var app = express();
+
+app.set('views', './views');
+app.set('view engine', 'jade');
+
+app.use(stormpath.init(app, {
+  website: true,
+  expand: {
+    customData: true
+  }
+}));
+
+app.get('/', function(req, res) {
+  res.render('login', {
+    title: 'Welcome'
+  });
+});
+
+app.use('/profile',stormpath.loginRequired,require('./routes/profile')());
+
+
+app.on('stormpath.ready',function(){
+  console.log('Stormpath Ready');
+  app.listen(3000);
+});
