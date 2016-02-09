dcf6308b11fdefb55d7f32a86a53c52016147dcc
jes97210 <jes97210@yahoo.com>
2016-02-08 16:48:53 -0800

    Just saving it...

--- src/comic.ts
+++ src/comic.ts
@@ -5,7 +5,7 @@ class Comic {
 
     _title: string;
     _publicView: boolean;
-    _panelLimit: number;
+    _panelLimit: number = 9;
     _panelList: Panel[];
     //NOTE: Just made it a list of strings, since we don't need to link the contributor name in any way right now,
     // and I assume that the list of contributors is stored in the database?
@@ -26,6 +26,16 @@ class Comic {
 
     }
 
+    addPanel() {
+        var i = this._panelList.length;
+        if (i < this._panelLimit ) {
+            this._panelList[i-1] = new Panel();
+
+        }
+        else {
+            // Return an error?
+        }
+    }
 }
 
 class Panel {
@@ -33,6 +43,11 @@ class Panel {
     _message: string;
     _charLimit: number;
     //Also will need various vars for the size restrictions.
+    private _sizelimit: number = 5;
+    private _fileformat: string = ".jpg";
+    private _defaultPanel: HTMLImageElement;
+    private _panelHeight: number = 426;
+    private _panelWidth: number = 331;
 
 
     // TODO: constructor for panel
