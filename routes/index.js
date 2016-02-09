b9325a9bca798b5d76efe38eb6a972201fb78363
ScottHenry <scotthenry95@gmail.com>
2016-02-08 16:08:36 -0800

    Testing index.js

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 var path    = require("path");
 
-
 /* GET login page(home). */
 router.get('/', function(req, res, next) {
   res.render('login', { title: 'login' });
@@ -13,12 +12,14 @@ router.get('/view', function(req, res, next) {
   res.sendFile(path.join(__dirname, '../views', 'view.html'));
 });
 
-/* GET login page. */
+/* GET edit page. */
 router.get('/edit', function(req, res, next) {
   res.sendFile(path.join(__dirname, '../views', 'login.jade'));
 });
 
-
-
+/* GET TEST page. */
+router.get('/test', function(req, res) {
+  res.render('test', { title: 'Hello, World!' });
+});
 
 module.exports = router;
