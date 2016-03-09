51b6f3d1c698352990a000664aa3aa6e70c14f2d
jes97210 <jes97210@yahoo.com>
2016-03-09 09:08:13 -0800

    Basics of Jasmine test framework. Make sure to install karma, jasmine, and request.

---
+++ test/comicthruindexspec.js
@@ -0,0 +1,17 @@
+/**
+ * Created by jes97210 on 3/9/16.
+ */
+var request = require (["request"]);
+
+var base_url = "http://localhost:3000/";
+
+describe("Anything to do with comics", function() {
+    describe("GET /", function() {
+        it("returns status code 200", function() {
+            request( {method: 'GET', uri: base_url}, function(response) {
+                expect(response.statusCode).toBe(200);
+                done();
+            });
+        });
+    });
+});
