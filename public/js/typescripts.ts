ed488397490ea5667f275543315534119138a109
tjxn <trevor@trevorjackson.ca>
2016-03-09 20:32:20 -0800

    Improved removal of Edit button on view page for Viewer.

--- public/js/typescripts.ts
+++ public/js/typescripts.ts
@@ -903,7 +903,7 @@ function checkIfViewer(){
 
         if(data.toString() == "Viewer"){
             var id = (<HTMLAnchorElement>  document.getElementById("Edit-Button"));
-            id.style.visibility = "hidden";
+            id.remove();
         }
 
 
